6e12e074c8be71a55c3dc63299ad85c2edc18d45
jackson <shinyjijo@live.com>
2018-03-03 23:52:12 +1000

    testing new method for browsing courses from memrise right in-app. in progress

--- helpLibs/memrise.py
+++ helpLibs/memrise.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 COURSE_URL = '/course/128133/dirty-russian-by-native-russian-speaker/'
+COURSES_URL = 'https://www.memrise.com/courses/english/languages/'
 CARD_COLUMNS = ("col_a", "col_b")
 
 import codecs, sys
@@ -8,6 +9,7 @@ import re
 import requests
 from bs4 import BeautifulSoup
 from lxml import html
+import traceback
 
 def lazy_property(fn):
     """Decorator that makes a property lazy-evaluated.
@@ -28,8 +30,149 @@ def get_soup(url, session):
     soup = BeautifulSoup(res.text, "html.parser")
     return soup
 
-class Course(object):
+class CourseBrowser(object):
+    def __init__(self):
+        payload = { "username": "MyPySrs", 
+                        "password": "MyPySrs", 
+                        "csrfmiddlewaretoken": "<TOKEN>"
+                        }
+        self.session =  requests.session()
+        login_url = 'https://www.memrise.com/login/'
+        result = self.session.get(login_url)
+        tree = html.fromstring(result.text)
+        authenticity_token = list(set(tree.xpath("//input[@name='csrfmiddlewaretoken']/@value")))[0]
+        payload['csrfmiddlewaretoken'] = authenticity_token
 
+        self.session.post(login_url, data = payload, headers = dict(referer=login_url)) 
+
+        self.courses_url = COURSES_URL
+        self.getLanguages()
+
+    def getLanguages(self):
+        coursesDict = {}
+        soup = get_soup(self.courses_url, self.session) 
+        soup = soup.find("ul",{'data-default-category-id':'569'})
+        #print(soup.find('a').text.strip())
+        
+        lis = []
+        uls = soup
+        indent = 0
+        count = -1
+        for ul in uls:
+            if not type(ul.find('a')) is int: lis.append((ul.find('a'), indent))
+            try:
+                for li in ul.findAll('li'):
+                    indent += 1
+                    if li.find("span"): indent -= 1; count = len([x for x in [x for x in li.children if not x == '\n'][1].children if not x == '\n']) + 1
+                    if count:
+                        count -= 1
+                    else:
+                        count -= 1
+                        indent -= 1
+                    if not 'data-category-id' in li.attrs: indent += 1
+                    if li.find('ul'):
+                        lis.append((li.find('a'), indent))
+                        if not count:
+                            indent += 1
+                        continue
+                    indent -= 1
+                    lis.append((li, indent))
+                indent -= 1
+            except Exception as e:
+                pass#print(traceback.format_exc())
+            #indent -= 1
+
+        allCat = {}
+        cur1 = None
+        cur2 = None
+        cur3 = None
+        for li in lis:
+            mi = li[0]#.text.strip()
+            #print('\t' * li[1] + li[0].text.strip())
+            if li[1] == 0:
+                if not mi in allCat: allCat[mi] = {}
+                cur1 = mi
+
+            if li[1] == 1:
+                if not mi in allCat[cur1]: allCat[cur1][mi] = []
+                cur2 = mi
+
+            if li[1] == 2:
+                if not mi in allCat[cur1][cur2]: allCat[cur1][cur2].append(mi)
+                cur3 = mi
+
+        for li in lis:
+            mi = li[0]#.text.strip()
+
+            if li[1] == 0:
+                cur1 = mi
+
+            if li[1] == 1:
+                cur2 = mi
+                if len(allCat[cur1][mi]) == 0:
+                    allCat[cur1][mi] = None
+        
+        #finished = False
+        #relo = {}
+        #cur = [soup]
+        #test = []
+        #sibs = []
+        # while len(cur) > 0:
+        #     try:
+        #         t = len(cur)-1
+        #         for i in [x for x in cur[len(cur)-1].children if not x == '\n']:
+        #             cur.append(i)
+        #             #test.append(i)
+        #             try:
+        #                 lasti = i.text.strip();
+        #                 if not '\n' in lasti:
+        #                     #print(relo[lasti], lasti)
+        #                     #if not mi in relo[lasti]: relo[lasti][mi] = {}
+        #                     #print(lasti)
+        #                     #lasti = i.text.strip()
+        #                     sibs = []
+        #             except Exception as e: print(e)
+        #         del cur[t]
+        #     except Exception as e:
+        #         relo[lasti] = cur[t].strip()
+        #         test.append(cur[t])
+        #         del cur[t]
+            
+        
+        #print(relo)
+            
+
+            
+
+
+        #for i in soup.children:
+        #    print(i)
+                    
+
+                #if not str(j) == "\n": print(j.prettify(), '1')
+            #for j in soup.find('li'):
+             #   print(i,j)
+            #for col in row.find_all('a'):
+             #   print(row, col)
+
+        #while len(soupSearch[len(soupSearch) - 1]) > 0:
+
+         #   soupSearch.append(soupSearch[len(soupSearch) - 1].find('a'))
+            
+            # try:
+            #     if not i.string.strip() == "":
+            #         if not i.string.strip() in coursesDict:
+            #             coursesDict[i.string.strip()] = []
+            # except:
+            #     pass
+            # for j in i:
+            #     try:
+            #         coursesDict[i.string.strip()].append(j.string.strip())
+            #     except:
+            #         pass
+        #print(coursesDict)
+
+class Course(object):
     def __init__(self, course_url):
         payload = { "username": "MyPySrs", 
                         "password": "MyPySrs", 
@@ -42,7 +185,7 @@ class Course(object):
         authenticity_token = list(set(tree.xpath("//input[@name='csrfmiddlewaretoken']/@value")))[0]
         payload['csrfmiddlewaretoken'] = authenticity_token
 
-        result = self.session.post(login_url, data = payload, headers = dict(referer=login_url)) 
+        self.session.post(login_url, data = payload, headers = dict(referer=login_url)) 
 
         match = re.match(r'^(.*)/(\d+)/?$', course_url)
         if match:
@@ -137,3 +280,5 @@ def main(url):
     sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
     course_url = COURSE_URL if len(sys.argv) < 2 else sys.argv[1]
     return dump_course(course_url=url)
+
+t = CourseBrowser()
